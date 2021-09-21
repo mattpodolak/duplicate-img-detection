e7c2f324787614b41566f250827f004e9d47b3e7
Matthew Podolak <mpodola2@gmail.com>
2021-09-21 11:28:09 -0400

    relocated tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,15 +6,15 @@ from app.api import app
 
 def test_single_upload():
   client = TestClient(app)
-  with open("./app/data/is_my_cat_normal.jpg", "rb") as f:
+  with open("./tests/data/is_my_cat_normal.jpg", "rb") as f:
     response = client.post('/api/add/image', files={'file':  f} )
     assert response.status_code == 201
     assert response.json() == {"added": ["is_my_cat_normal.jpg"]}
 
 def test_multiple_uploads():
   client = TestClient(app)
-  multiple_files =[('files', open('./app/data/is_my_cat_normal.jpg', 'rb')), 
-    ('files', open('./app/data/is_my_cat_normal_copy.jpg', 'rb'))]
+  multiple_files =[('files', open('./tests/data/is_my_cat_normal.jpg', 'rb')), 
+    ('files', open('./tests/data/is_my_cat_normal_copy.jpg', 'rb'))]
   response = client.post('/api/add/images', files=multiple_files)
   assert response.status_code == 201
   assert response.json() == {"added": ["is_my_cat_normal.jpg", "is_my_cat_normal_copy.jpg"]}
@@ -27,14 +27,14 @@ def test_check_duplicate():
   index.reset()
 
   # load image into index
-  img = Image.open("./app/data/is_my_cat_normal.jpg")
+  img = Image.open("./tests/data/is_my_cat_normal.jpg")
   img_hash = hash_image(img, hash_size)
   index.add(img_hash)
   save_index(index, hash_size=hash_size)
 
   client = TestClient(app)
 
-  with open("./app/data/is_my_cat_normal_copy.jpg", "rb") as f:
+  with open("./tests/data/is_my_cat_normal_copy.jpg", "rb") as f:
     test_file ={'file':  f} 
     response = client.post('/api/check', files=test_file, data={'dist': 5})
     assert response.status_code == 200
@@ -48,14 +48,14 @@ def test_check_non_duplicate():
   index.reset()
 
   # load image into index
-  img = Image.open("./app/data/doge.png")
+  img = Image.open("./tests/data/doge.png")
   img_hash = hash_image(img, hash_size)
   index.add(img_hash)
   save_index(index, hash_size=hash_size)
 
   client = TestClient(app)
   
-  with open("./app/data/is_my_cat_normal_copy.jpg", "rb") as f:
+  with open("./tests/data/is_my_cat_normal_copy.jpg", "rb") as f:
     test_file ={'file':  f} 
     response = client.post('/api/check', files=test_file, data={'dist': 5})
     assert response.status_code == 200
