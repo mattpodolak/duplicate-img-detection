65cc04be0b1a98d6de38af94ab13792dda5b3e3d
Matthew Podolak <mpodola2@gmail.com>
2021-09-21 11:28:17 -0400

    updated docs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import uvicorn
-from app.api import app
-
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080, log_level='info')
+    uvicorn.run("app.api:app", host='0.0.0.0', port=8080, log_level='info', reload=True)
